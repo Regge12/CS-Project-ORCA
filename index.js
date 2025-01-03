2d62b0318161060a9af51b714a9e6a23905f783a
Regge12 <dokafor10x@gmail.com>
2025-01-03 01:33:15 +0000

    Creation of the HTML file, and website styling (temp)

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express'); // Importing the Express framework
 const { createServer } = require('node:http'); // Importing the createServer function from the http module
+const { join } = require('node:path'); // Importing join function from the path module 
 
 const app = express(); // Creates and instance of Express
 const server = createServer(app); // Creates a HTTP server
-//Express (app) will handel all incoming HTTP requests
+// Express (app) will handel all incoming HTTP requests
 
 app.get('/', (req, res) => {
-    res.send('<h1>Helloooo World</h1><p>Whats Good MY G</p>');
+    res.sendFile(join(__dirname, 'index.html'));
 })
-// On request of the server, the server will responds with this^
+// On request of the server, the server will responds with the HTML file (index.html)
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
