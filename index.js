0f683446b3fb4778c16cac414ec6fca7f993d68b
Regge12 <dokafor10x@gmail.com>
2025-01-03 01:50:53 +0000

    Test - When user disconnects

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
 })
 // When there is a connection to the server this event will fire
 
